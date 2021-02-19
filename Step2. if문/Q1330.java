e45b8a35d8f9dba8fbe1f2cfb8cee65be55d589f
Seoan Yoon <79207743+annyoon@users.noreply.github.com>
2021-02-19 23:47:47 +0900

    Add files via upload

---
+++ Step2. if문/Q1330.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q1330 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		int b = s.nextInt();
+		
+		if(a > b) System.out.println(">");
+		else if(a < b) System.out.println("<");
+		else System.out.println("==");
+	}
+}
