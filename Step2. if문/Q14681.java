c5e90ca02f789c0604216ecad2c5b441a11c66bb
Seoan Yoon <79207743+annyoon@users.noreply.github.com>
2021-02-22 23:24:45 +0900

    Add files via upload

---
+++ Step2. if문/Q14681.java
@@ -0,0 +1,28 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q14681 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int x = s.nextInt();
+		int y = s.nextInt();
+		
+		if(x > 0) {
+			if(y > 0) {
+				System.out.println("1");
+			}
+			else {
+				System.out.println("4");
+			}
+		}
+		else {
+			if(y > 0) {
+				System.out.println("2");
+			}
+			else {
+				System.out.println("3");
+			}
+		}
+	}
+}
